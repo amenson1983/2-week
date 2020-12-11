1be574723c4e613852a47f151ecb3124a4f8c58e
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-11 21:03:01 +0200

    First commit II

---
+++ 2 week/dictionary.py
@@ -0,0 +1,16 @@
+number = input("Put your number"),
+mapping = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
+
+}
+
+print(a)
