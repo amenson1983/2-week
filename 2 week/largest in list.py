1155a30ad34b9fad79b54d2e41d29c45207070c9
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-11 18:13:36 +0200

    First commit II

---
+++ 2 week/largest in list.py
@@ -0,0 +1,6 @@
+list = range(0,30,3)
+biggest = 0
+for num in list:
+    if num > biggest:
+        biggest = num
+print(biggest)
---
+++ 2 week/natik_stars.py
@@ -0,0 +1,7 @@
+number = int(input("Put the number: "))
+one = 1
+one_list = [2,2,2,2,2,2,2,2]
+if number == one:
+    for num in one_list:
+        print(("*" * num))
+
