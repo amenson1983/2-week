7e0d14fa7891acacdeec6062e0bd3b290c8b2df2
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-14 17:12:47 +0200

    1 task

---
+++ _2_week/Homework_2_week/Algoritmic_tranee_4.py
@@ -0,0 +1,8 @@
+count = 0
+cummulative =0
+a = int(input("Put the number: "))
+while count <10:
+    count +=1
+    cummulative += a
+print("Number of attempts: ", count)
+print("Sum: ", cummulative)
