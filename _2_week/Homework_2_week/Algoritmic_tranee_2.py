6f6c41f2b41c454d60f65ea06200b9bdf08e70fb
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-14 16:57:33 +0200

    1 task

---
+++ _2_week/Homework_2_week/Algoritmic_tranee_2.py
@@ -0,0 +1,7 @@
+cont = "y"
+while cont == "y":
+    a = int(input("Input figure a:"))
+    b = int(input("Input figure b:"))
+    c = a+b
+    print("The sum is: ", c)
+    cont = input("Would you like to contionue? 'type 'y' if yes': ")
