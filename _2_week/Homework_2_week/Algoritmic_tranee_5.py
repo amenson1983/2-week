a314eb3f61031ef6b7b0de504628568feef923d7
amenson1983 <75535151+amenson1983@users.noreply.github.com>
2020-12-15 10:12:12 +0200

    1 task

---
+++ _2_week/Homework_2_week/Algoritmic_tranee_5.py
@@ -0,0 +1,6 @@
+num_str = 10
+num_sym = 15
+for st in range(num_str):
+        print("#" * num_sym)
+            
+
---
+++ _2_week/Homework_2_week/Algoritmic_tranee_6.py
@@ -0,0 +1,8 @@
+numy = 30
+sum_ = 0
+if numy > 0:
+    for x in range(1,31,1):
+        sum_ += x/numy
+        numy -=1
+    print(sum_)
+else: print(")))")
